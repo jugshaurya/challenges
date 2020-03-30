cf9c810a6775403ed5de7fc63088d3b284788740
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-03-30 15:38:45 +0530

    :code: march lunctime

--- march-lunchtime/c.py
+++ march-lunchtime/c.py
@@ -13,10 +13,10 @@ while(t):
     d = B % 10
     if(A < 10 and B < 10):
         print(A + B)
-    # elif(A < 10 and B >= 10):
-    #     print(max(c+b*10+d, d+c*10+b, b+c*10+d))
-    # elif(B < 10 and A >= 10):
-    #     print(max(a+b*10+d, d+b*10+a, a+d*10+b))
+    elif(A < 10 and B >= 10):
+        print(max(c+b*10+d, d+c*10+b, b+c*10+d))
+    elif(B < 10 and A >= 10):
+        print(max(a*10+b+d, d*10+b+a, a*10+d+b))
     else:
-        print(max(d*10+b+c*10+a, c*10+b+a*10+d, a*10+c+b*10+d, a*10+d+c*10+b))
+        print(max(d*10+b+c*10+a, c*10+b+a*10+d, a*10+c+b*10+d, a*10+d+c*10+b, A+B))
     t -= 1
