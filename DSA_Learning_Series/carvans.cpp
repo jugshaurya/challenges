eae68a2205ceb8794419b059a1f3b26f7771766c
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-03-31 20:26:09 +0530

    :sheep: Added carvans and smart_phone question of DSA Series

--- DSA_Learning_Series/carvans.cpp
+++ DSA_Learning_Series/carvans.cpp
@@ -21,6 +21,7 @@ using namespace std;
 #define mk(arr,n,type)	type *arr=new type[n];
 #define w(x)            int x; cin>>x; while(x--)
 #define pw(b,p)         pow(b,p) + 0.1
+#define endl			"\n"
 mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
@@ -34,11 +35,42 @@ void fastIO(){
 	#endif
 }
 
-
 int32_t main(){
 	fastIO();
 	/** code here */
-
-
+	int arr[10000]={0};
+	w(x){
+		int activities;
+		string origin;
+		cin >> activities >> origin;
+		string type;
+		int rank;
+		int result = 0;
+		for (int i = 0; i < activities; ++i){
+			cin>>type;
+			if(type=="CONTEST_WON"){
+				cin>>rank;
+				if(rank<=20){
+					result += 300 + 20 - rank;
+				}else{
+					result += 300;
+				}
+			}else if (type=="BUG_FOUND") {
+				cin>>rank;
+				result +=rank;
+			}else{
+				if(type=="TOP_CONTRIBUTOR"){
+					result += 300;
+				}else{
+					// CONTEST_HOSTED
+					result += 50;
+				}
+			}
+		}
+		if(origin =="INDIAN"){
+			cout<<result/200<<endl;
+		}else
+			cout<<result/400<<endl;
+	}
 	return 0;
 }
---
+++ DSA_Learning_Series/smart_phone.cpp
@@ -0,0 +1,66 @@
+// 5
+// 40
+// 3
+// 65
+// 33
+// 21
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int 			long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007 // 1e9+7
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)	type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pw(b,p)         pow(b,p) + 0.1
+#define endl			"\n"
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+
+void fastIO(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	#ifndef ONLINE_JUDGE	
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int32_t main(){
+	fastIO();
+	/** code here */
+	vi budget;
+	int n; cin>>n;
+	int num;
+	for (int i = 0; i < n; ++i){
+		cin>>num;
+		budget.pb(num);
+	}
+
+	sort(budget.begin(), budget.end());
+
+	int result = INT_MIN;
+	for (int i = 0; i < n; ++i){
+		int revenue = budget[i]*(n-i);
+		result = max(result,revenue);
+	}
+
+	cout<<result<<endl;
+	return 0;
+}
